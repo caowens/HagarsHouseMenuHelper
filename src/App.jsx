a655884daf23ad4be8b889b1737de9da4fcb7f37
caowens <caowens555@yahoo.com>
2024-06-28 19:32:37 -0400

    Install material tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { Button } from "@material-tailwind/react";
+
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Button>Click me</Button>
   )
 }
 
