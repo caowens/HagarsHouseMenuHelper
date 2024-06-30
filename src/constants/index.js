5d9858cf013ee28bbb3d43ec46c2fa17c957cce6
caowens <caowens555@yahoo.com>
2024-06-30 16:37:39 -0400

    Make mock data for the cards

---
+++ src/constants/index.js
@@ -0,0 +1,494 @@
+const menus = [
+    {
+        "adultPortion": [
+            {
+                "ingredient": "Turkey",
+                "amount": 0.375,
+                "measurement": "lbs"
+            },
+            {
+                "ingredient": "Green Beans",
+                "amount": 4.0,
+                "measurement": "oz"
+            },
+            {
+                "ingredient": "Stuffing",
+                "amount": 4.0,
+                "measurement": "oz"
+            },
+            {
+                "ingredient": "Mashed Potatoes",
+                "amount": 0.75,
+                "measurement": "cup"
+            },
+            {
+                "ingredient": "Apple Pie",
+                "amount": 0.125,
+                "measurement": "Pie"
+            },
+            {
+                "ingredient": "Juice",
+                "amount": 0.125,
+                "measurement": "64 fl oz bottle"
+            }
+        ],
+        "portion1": [
+            {
+                "ingredient": "Turkey",
+                "amount": 0.125,
+                "measurement": "lbs"
+            },
+            {
+                "ingredient": "Green Beans",
+                "amount": 2.0,
+                "measurement": "oz"
+            },
+            {
+                "ingredient": "Stuffing",
+                "amount": 0.0,
+                "measurement": "oz"
+            },
+            {
+                "ingredient": "Mashed Potatoes",
+                "amount": 0.25,
+                "measurement": "cup"
+            },
+            {
+                "ingredient": "Apple Pie",
+                "amount": 0.0625,
+                "measurement": "Pie"
+            },
+            {
+                "ingredient": "Juice",
+                "amount": 0.046875,
+                "measurement": "64 fl oz bottle"
+            }
+        ],
+        "portion2": [
+            {
+                "ingredient": "Turkey",
+                "amount": 0.1875,
+                "measurement": "lbs"
+            },
+            {
+                "ingredient": "Green Beans",
+                "amount": 2.0,
+                "measurement": "oz"
+            },
+            {
+                "ingredient": "Stuffing",
+                "amount": 2.0,
+                "measurement": "oz"
+            },
+            {
+                "ingredient": "Mashed Potatoes",
+                "amount": 0.375,
+                "measurement": "cup"
+            },
+            {
+                "ingredient": "Apple Pie",
+                "amount": 0.125,
+                "measurement": "Pie"
+            },
+            {
+                "ingredient": "Juice",
+                "amount": 0.09375,
+                "measurement": "64 fl oz bottle"
+            }
+        ],
+        "portion3": [
+            {
+                "ingredient": "Turkey",
+                "amount": 0.1875,
+                "measurement": "lbs"
+            },
+            {
+                "ingredient": "Green Beans",
+                "amount": 2.0,
+                "measurement": "oz"
+            },
+            {
+                "ingredient": "Stuffing",
+                "amount": 2.0,
+                "measurement": "oz"
+            },
+            {
+                "ingredient": "Mashed Potatoes",
+                "amount": 0.375,
+                "measurement": "cup"
+            },
+            {
+                "ingredient": "Apple Pie",
+                "amount": 0.125,
+                "measurement": "Pie"
+            },
+            {
+                "ingredient": "Juice",
+                "amount": 0.09375,
+                "measurement": "64 fl oz bottle"
+            }
+        ]
+    },
+    {
+        "adultPortion": [
+            {
+                "ingredient": "Meat Pizza",
+                "amount": 0.25,
+                "measurement": "Large pizza"
+            },
+            {
+                "ingredient": "Veggie Pizza",
+                "amount": 0.125,
+                "measurement": "Medium pizza"
+            },
+            {
+                "ingredient": "Cheese Pizza",
+                "amount": 0.0,
+                "measurement": "Large pizza"
+            },
+            {
+                "ingredient": "Lettuce (Salad)",
+                "amount": 0.3333333333,
+                "measurement": "6 oz bag"
+            },
+            {
+                "ingredient": "Tomatoes (Salad)",
+                "amount": 0.5,
+                "measurement": "tomato"
+            },
+            {
+                "ingredient": "Carrots (Salad)",
+                "amount": 0.1,
+                "measurement": "2 lb bag"
+            },
+            {
+                "ingredient": "Croutons",
+                "amount": 0.06666666667,
+                "measurement": "5 oz Bag"
+            },
+            {
+                "ingredient": "Juice",
+                "amount": 0.125,
+                "measurement": "64 fl oz bottle"
+            }
+        ],
+        "portion1": [
+            {
+                "ingredient": "Meat Pizza",
+                "amount": 0.125,
+                "measurement": "Large pizza"
+            },
+            {
+                "ingredient": "Veggie Pizza",
+                "amount": 0.0,
+                "measurement": "Medium pizza"
+            },
+            {
+                "ingredient": "Cheese Pizza",
+                "amount": 0.125,
+                "measurement": "Large pizza"
+            },
+            {
+                "ingredient": "Lettuce (Salad)",
+                "amount": 0.0,
+                "measurement": "6 oz bag"
+            },
+            {
+                "ingredient": "Tomatoes (Salad)",
+                "amount": 0.0,
+                "measurement": "tomato"
+            },
+            {
+                "ingredient": "Carrots (Salad)",
+                "amount": 0.0,
+                "measurement": "2 lb bag"
+            },
+            {
+                "ingredient": "Croutons",
+                "amount": 0.0,
+                "measurement": "5 oz Bag"
+            },
+            {
+                "ingredient": "Juice",
+                "amount": 0.046875,
+                "measurement": "64 fl oz bottle"
+            }
+        ],
+        "portion2": [
+            {
+                "ingredient": "Meat Pizza",
+                "amount": 0.125,
+                "measurement": "Large pizza"
+            },
+            {
+                "ingredient": "Veggie Pizza",
+                "amount": 0.0,
+                "measurement": "Medium pizza"
+            },
+            {
+                "ingredient": "Cheese Pizza",
+                "amount": 0.25,
+                "measurement": "Large pizza"
+            },
+            {
+                "ingredient": "Lettuce (Salad)",
+                "amount": 0.3333333333,
+                "measurement": "6 oz bag"
+            },
+            {
+                "ingredient": "Tomatoes (Salad)",
+                "amount": 0.25,
+                "measurement": "tomato"
+            },
+            {
+                "ingredient": "Carrots (Salad)",
+                "amount": 0.1,
+                "measurement": "2 lb bag"
+            },
+            {
+                "ingredient": "Croutons",
+                "amount": 0.06666666667,
+                "measurement": "5 oz Bag"
+            },
+            {
+                "ingredient": "Juice",
+                "amount": 0.09375,
+                "measurement": "64 fl oz bottle"
+            }
+        ],
+        "portion3": [
+            {
+                "ingredient": "Meat Pizza",
+                "amount": 0.125,
+                "measurement": "Large pizza"
+            },
+            {
+                "ingredient": "Veggie Pizza",
+                "amount": 0.0,
+                "measurement": "Medium pizza"
+            },
+            {
+                "ingredient": "Cheese Pizza",
+                "amount": 0.25,
+                "measurement": "Large pizza"
+            },
+            {
+                "ingredient": "Lettuce (Salad)",
+                "amount": 0.3333333333,
+                "measurement": "6 oz bag"
+            },
+            {
+                "ingredient": "Tomatoes (Salad)",
+                "amount": 0.25,
+                "measurement": "tomato"
+            },
+            {
+                "ingredient": "Carrots (Salad)",
+                "amount": 0.1,
+                "measurement": "2 lb bag"
+            },
+            {
+                "ingredient": "Croutons",
+                "amount": 0.06666666667,
+                "measurement": "5 oz Bag"
+            },
+            {
+                "ingredient": "Juice",
+                "amount": 0.09375,
+                "measurement": "64 fl oz bottle"
+            }
+        ]
+    },
+    {
+        "adultPortion": [
+            {
+                "ingredient": "Spaghetti (Meat Sauce)",
+                "amount": 0.25,
+                "measurement": "24 oz Jar"
+            },
+            {
+                "ingredient": "Spaghetti (Sauce, No Meat)",
+                "amount": 0.125,
+                "measurement": "24 oz Jar"
+            },
+            {
+                "ingredient": "Spaghetti Pasta",
+                "amount": 0.1666666667,
+                "measurement": "16 oz dried pasta"
+            },
+            {
+                "ingredient": "Garlic Bread",
+                "amount": 0.1666666667,
+                "measurement": "Loaf"
+            },
+            {
+                "ingredient": "Lettuce (Salad)",
+                "amount": 0.3333333333,
+                "measurement": "6 oz bag"
+            },
+            {
+                "ingredient": "Tomatoes (Salad)",
+                "amount": 0.5,
+                "measurement": "tomato"
+            },
+            {
+                "ingredient": "Carrots (Salad)",
+                "amount": 0.1,
+                "measurement": "2 lb bag"
+            },
+            {
+                "ingredient": "Croutons",
+                "amount": 0.06666666667,
+                "measurement": "5 oz Bag"
+            },
+            {
+                "ingredient": "Tea",
+                "amount": 0.125,
+                "measurement": "gallon"
+            }
+        ],
+        "portion1": [
+            {
+                "ingredient": "Spaghetti (Meat Sauce)",
+                "amount": 0.0,
+                "measurement": "24 oz Jar"
+            },
+            {
+                "ingredient": "Spaghetti (Sauce, No Meat)",
+                "amount": 0.1,
+                "measurement": "24 oz Jar"
+            },
+            {
+                "ingredient": "Spaghetti Pasta",
+                "amount": 0.08333333333,
+                "measurement": "16 oz dried pasta"
+            },
+            {
+                "ingredient": "Garlic Bread",
+                "amount": 0.0,
+                "measurement": "Loaf"
+            },
+            {
+                "ingredient": "Lettuce (Salad)",
+                "amount": 0.0,
+                "measurement": "6 oz bag"
+            },
+            {
+                "ingredient": "Tomatoes (Salad)",
+                "amount": 0.0,
+                "measurement": "tomato"
+            },
+            {
+                "ingredient": "Carrots (Salad)",
+                "amount": 0.0,
+                "measurement": "2 lb bag"
+            },
+            {
+                "ingredient": "Croutons",
+                "amount": 0.0,
+                "measurement": "5 oz Bag"
+            },
+            {
+                "ingredient": "Tea",
+                "amount": 0.046875,
+                "measurement": "gallon"
+            }
+        ],
+        "portion2": [
+            {
+                "ingredient": "Spaghetti (Meat Sauce)",
+                "amount": 0.2,
+                "measurement": "24 oz Jar"
+            },
+            {
+                "ingredient": "Spaghetti (Sauce, No Meat)",
+                "amount": 0.1,
+                "measurement": "24 oz Jar"
+            },
+            {
+                "ingredient": "Spaghetti Pasta",
+                "amount": 0.125,
+                "measurement": "16 oz dried pasta"
+            },
+            {
+                "ingredient": "Garlic Bread",
+                "amount": 0.08333333333,
+                "measurement": "Loaf"
+            },
+            {
+                "ingredient": "Lettuce (Salad)",
+                "amount": 0.3333333333,
+                "measurement": "6 oz bag"
+            },
+            {
+                "ingredient": "Tomatoes (Salad)",
+                "amount": 0.25,
+                "measurement": "tomato"
+            },
+            {
+                "ingredient": "Carrots (Salad)",
+                "amount": 0.1,
+                "measurement": "2 lb bag"
+            },
+            {
+                "ingredient": "Croutons",
+                "amount": 0.06666666667,
+                "measurement": "5 oz Bag"
+            },
+            {
+                "ingredient": "Tea",
+                "amount": 0.09375,
+                "measurement": "gallon"
+            }
+        ],
+        "portion3": [
+            {
+                "ingredient": "Spaghetti (Meat Sauce)",
+                "amount": 0.2,
+                "measurement": "24 oz Jar"
+            },
+            {
+                "ingredient": "Spaghetti (Sauce, No Meat)",
+                "amount": 0.1,
+                "measurement": "24 oz Jar"
+            },
+            {
+                "ingredient": "Spaghetti Pasta",
+                "amount": 0.125,
+                "measurement": "16 oz dried pasta"
+            },
+            {
+                "ingredient": "Garlic Bread",
+                "amount": 0.08333333333,
+                "measurement": "Loaf"
+            },
+            {
+                "ingredient": "Lettuce (Salad)",
+                "amount": 0.3333333333,
+                "measurement": "6 oz bag"
+            },
+            {
+                "ingredient": "Tomatoes (Salad)",
+                "amount": 0.25,
+                "measurement": "tomato"
+            },
+            {
+                "ingredient": "Carrots (Salad)",
+                "amount": 0.1,
+                "measurement": "2 lb bag"
+            },
+            {
+                "ingredient": "Croutons",
+                "amount": 0.06666666667,
+                "measurement": "5 oz Bag"
+            },
+            {
+                "ingredient": "Tea",
+                "amount": 0.09375,
+                "measurement": "gallon"
+            }
+        ]
+    }
+];
+
+export {menus};
