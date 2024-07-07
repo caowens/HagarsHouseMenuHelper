843a0cbd98a64268bf5803b590a4ec3953fecb7d
caowens <caowens555@yahoo.com>
2024-07-06 20:09:33 -0400

    Make landing page mobile responsive

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -10,6 +10,7 @@ import { useState, useContext } from "react";
 import "../App.css";
 import { Link, useNavigate } from "react-router-dom";
 import { FiltersContext } from "../FiltersContext";
+import { useMediaQuery } from "@mui/material";
 // import { db, getMenus } from "../firebase/firebase";
 
 export default function Landing(params) {
@@ -23,6 +24,8 @@ export default function Landing(params) {
   const handleSearch = () => {
     navigate("/menus");
   };
+
+  const isMdUp = useMediaQuery('(min-width: 768px)');
 
   // const handleMenus = async () => {
   //   try {
@@ -70,14 +73,14 @@ export default function Landing(params) {
   // }
 
   return (
-    <div className="flex flex-col justify-center items-center gap-10 my-20">
+    <div className="flex flex-col justify-center items-center sm:gap-10 mt-20 sm:my-20">
       <div className="flex w-full flex-col lg:flex-row">
-        <div className="h-full w-full flex-grow place-items-center mr-10">
-          <div className="h-80 flex flex-col justify-center items-end gap-4">
-            <Typography variant="h4" className="text-end">
+        <div className="h-full w-full flex-grow place-items-center sm:mr-10">
+          <div className="h-80 flex flex-col justify-center items-center sm:items-end gap-4">
+            <Typography variant="h4" className="text-center sm:text-end">
               How many?
             </Typography>
-            <div className="flex gap-6 items-center">
+            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center">
               <Typography className="w-full" variant="h6">
                 Adult
               </Typography>
@@ -91,7 +94,7 @@ export default function Landing(params) {
                 }}
               />
             </div>
-            <div className="flex gap-6 items-center">
+            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center">
               <Typography className="w-full" variant="h6">
                 Child aged 0-3
               </Typography>
@@ -105,7 +108,7 @@ export default function Landing(params) {
                 }}
               />
             </div>
-            <div className="flex gap-6 items-center">
+            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center">
               <Typography className="w-full" variant="h6">
                 Child aged 4-7
               </Typography>
@@ -119,7 +122,7 @@ export default function Landing(params) {
                 }}
               />
             </div>
-            <div className="flex gap-6 items-center">
+            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center">
               <Typography className="w-full" variant="h6">
                 Child aged 8-11
               </Typography>
@@ -136,21 +139,21 @@ export default function Landing(params) {
           </div>
         </div>
 
-        <span className="vertical-line"></span>
+        {isMdUp ? <span className="vertical-line"></span> : <hr />}
 
-        <div className="h-full w-full flex-grow place-items-center ml-10">
-          <div className="h-80 flex flex-col justify-center items-start gap-4">
-            <div className="flex items-center">
+        <div className="h-full w-full flex-grow place-items-center mt-6 sm:ml-10">
+          <div className="h-72 sm:h-80 flex flex-col justify-center items-center sm:items-start gap-4">
+            <div className="flex items-center flex-col sm:flex-row">
               <Typography className="w-32" variant="h6">
                 Food Allergies
               </Typography>
-              <div className="gap-2 w-64 flex">
+              <div className="gap-2 flex">
                 <Checkbox label="Dairy" checked={filters.dairy} onChange={(e) => handleChange("dairy", e.target.checked)} />
                 <Checkbox label="Gluten" checked={filters.gluten} onChange={(e) => handleChange("gluten", e.target.checked)} />
                 <Checkbox label="Nuts" checked={filters.nuts} onChange={(e) => handleChange("nuts", e.target.checked)} />
               </div>
             </div>
-            <div className="flex gap-6 items-center">
+            <div className="flex gap-6 items-center flex-col sm:flex-row">
               <Typography className="w-full" variant="h6">
                 Dietary Needs
               </Typography>
@@ -173,7 +176,7 @@ export default function Landing(params) {
         </div>
       </div>
 
-        <Button onClick={handleSearch}>Find menus</Button>
+        <Button className="-mt-4" onClick={handleSearch}>Find menus</Button>
     </div>
   );
 }
