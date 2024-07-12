30670462a8bc653d4c9832674c45431dd2e723e6
caowens <caowens555@yahoo.com>
2024-07-11 21:40:36 -0400

    Remove filters from landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -75,12 +75,12 @@ export default function Landing(params) {
   return (
     <div className="flex flex-col justify-center items-center sm:gap-10 mt-20 sm:my-20">
       <div className="flex w-full flex-col lg:flex-row">
-        <div className="h-full w-full flex-grow place-items-center sm:mr-10">
-          <div className="h-80 flex flex-col justify-center items-center sm:items-end gap-4">
-            <Typography variant="h4" className="text-center sm:text-end">
+        <div className="h-full w-full flex-grow place-items-center">
+          <div className="h-80 flex flex-col justify-center items-center gap-4">
+            <Typography variant="h4" className="text-center">
               How many?
             </Typography>
-            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center">
+            <div className="flex flex-col gap-4 sm:gap-6 items-center">
               <Typography className="w-full" variant="h6">
                 Adult
               </Typography>
@@ -94,7 +94,7 @@ export default function Landing(params) {
                 }}
               />
             </div>
-            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center">
+            <div className="flex flex-col gap-4 sm:gap-6 items-center">
               <Typography className="w-full" variant="h6">
                 Child aged 0-3
               </Typography>
@@ -108,7 +108,7 @@ export default function Landing(params) {
                 }}
               />
             </div>
-            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center">
+            <div className="flex flex-col gap-4 sm:gap-6 items-center">
               <Typography className="w-full" variant="h6">
                 Child aged 4-7
               </Typography>
@@ -122,7 +122,7 @@ export default function Landing(params) {
                 }}
               />
             </div>
-            <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center">
+            <div className="flex flex-col gap-4 sm:gap-6 items-center">
               <Typography className="w-full" variant="h6">
                 Child aged 8-11
               </Typography>
@@ -138,57 +138,9 @@ export default function Landing(params) {
             </div>
           </div>
         </div>
-
-        {isMdUp ? <span className="vertical-line"></span> : <hr />}
-
-        <div className="h-full w-full flex-grow place-items-center mt-6 sm:ml-10">
-          <div className="h-72 sm:h-80 flex flex-col justify-center items-center sm:items-start gap-4">
-            <div className="flex items-center flex-col sm:flex-row">
-              <Typography className="w-32" variant="h6">
-                Food Allergies
-              </Typography>
-              <div className="gap-2 flex">
-                <Checkbox
-                  label="Dairy"
-                  checked={filters.dairy}
-                  onChange={(e) => handleChange("dairy", e.target.checked)}
-                />
-                <Checkbox
-                  label="Gluten"
-                  checked={filters.gluten}
-                  onChange={(e) => handleChange("gluten", e.target.checked)}
-                />
-                <Checkbox
-                  label="Nuts"
-                  checked={filters.nuts}
-                  onChange={(e) => handleChange("nuts", e.target.checked)}
-                />
-              </div>
-            </div>
-            <div className="flex gap-6 items-center flex-col sm:flex-row">
-              <Typography className="w-full" variant="h6">
-                Dietary Needs
-              </Typography>
-              <Select
-                label="Select Need"
-                value={filters.dietary}
-                onChange={(val) => handleChange("dietary", val)}
-              >
-                <Option value="none">None</Option>
-                <Option value="vegetarian">Vegetarian</Option>
-                <Option value="vegan">Vegan</Option>
-                <Option value="pescatarian">Pescatarian</Option>
-              </Select>
-            </div>
-
-            {/* <Button onClick={handleMenus}>Print menus</Button>
-            <Button onClick={addMenu}>Add menu</Button>
-            <Button onClick={addAllMenus}>Add all menus</Button> */}
-          </div>
-        </div>
       </div>
 
-      <Button className="-mt-4" onClick={handleSearch}>
+      <Button className="mt-20" onClick={handleSearch}>
         Find menus
       </Button>
     </div>
